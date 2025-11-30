348e8dc5b17c69d89137e2749483756506e6ce1a
FrancoDeavux <166407581+FrancoDevaux@users.noreply.github.com>
2025-11-30 16:46:07 -0300

    modif estructura

--- src/services/product.Service.ts
+++ src/services/product.Service.ts
@@ -1,10 +1,10 @@
 import Product from '../models/product'
 import data from '../mock/productMock.json'
-// Obtener todos los productos con paginación
+
 export const getAllProducts = async () => {
   return data
 }
-// Obtener un producto por su ID
+
 export const getProductById = async (id: number) => {
   const product = data.find(p => p.id_product === id)
   if (!product) {
@@ -13,16 +13,16 @@ export const getProductById = async (id: number) => {
   return product
 }
 
-// Crear un nuevo producto
+// Crear
 export const createProduct = async (productData: Product) => {
   const newProduct = await new Product(productData)
   data.push(newProduct)
   return newProduct
 }
 
-// Actualizar un producto existente
+// Actualizar 
 export const updateProduct = async (id: number, productData: Product) => {
-  const producto = await getProductById(id) // Reutilizamos la función para verificar si existe
+  const producto = await getProductById(id) 
   if (producto) {
     producto.name = productData.name
     producto.description = productData.description
@@ -33,9 +33,9 @@ export const updateProduct = async (id: number, productData: Product) => {
   return producto
 }
 
-// Eliminar un producto
+// Eliminar
 export const deleteProduct = async (id: number) => {
-  const product = await getProductById(id) // Verificamos si existe
+  const product = await getProductById(id) 
   if (product) {
     const a = data.findIndex(d => d.id_product === id)
     if (a >= 0) data.splice(a, 1)
