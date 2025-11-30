3f2d31801afc72643a33db46f0412c849473adad
Ivo Depari <ivodepari1302@hotmail.com>
2025-11-30 17:48:10 -0300

    correccion de fecha ygenerated_by_user

--- src/models/report.ts
+++ src/models/report.ts
@@ -3,10 +3,10 @@ import ReportAttributes from './Interface/reportAttributes'
 class Report implements ReportAttributes {
   public id_report!: number
   public readonly date_generated!: string
-  public generated_by_user!: number
+  public generated_by_user!: string
   constructor(reporteData: Report) {
     this.id_report = reporteData.id_report
-    this.date_generated = '23/10/25'
+    this.date_generated = '23/10/2025'
     this.generated_by_user = reporteData.generated_by_user
   }
 }
