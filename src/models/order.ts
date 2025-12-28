6207c55f78cfca6f1e7c8aac87dcf85ea01e7a3f
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2025-12-28 15:04:34 -0300

    modificacion de order y orderDetail para adaptarlo a la base de datos

--- src/models/order.ts
+++ src/models/order.ts
@@ -1,17 +1,53 @@
+import {DataTypes, Model, Optional} from 'sequelize'
 import OrderAttributes from './Interface/orderAttributes'
-class Order implements OrderAttributes {
-  public id_order!: number
-  public id_user!: string
-  public date!: string
-  public state!: string
-  public total!: number
-  constructor(orderData: Order) {
-    this.id_order = orderData.id_order
-    this.id_user = orderData.id_user
-    this.date = orderData.date
-    this.state = orderData.state
-    this.total = orderData.total
-  }
+import sequelize from '../database/db'
+type orderCreationAttributes = Optional<OrderAttributes, 'id_order'>
+class Order
+  extends Model<OrderAttributes, orderCreationAttributes>
+  implements OrderAttributes
+{
+  declare id_order: number
+  declare id_user: number
+  declare date: Date
+  declare state: string
+  declare total: number
 }
+Order.init(
+  {
+    id_order: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true,
+    },
+    id_user: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id_user',
+      },
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'pendiente',
+    },
+    total: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'Orders',
+    timestamps: true,
+    underscored: true,
+  },
+)
 
 export default Order
