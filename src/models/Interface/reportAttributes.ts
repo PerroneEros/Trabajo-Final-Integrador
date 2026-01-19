ae7def97458d12b4add662a152323bfd270d7617
Ivo Depari <ivodepari1302@hotmail.com>
2026-01-19 15:41:17 -0300

    modifcacion del modelo y atributos de report

--- src/models/Interface/reportAttributes.ts
+++ src/models/Interface/reportAttributes.ts
@@ -1,5 +1,9 @@
 interface ReportAttributes {
   id_report: number
   generated_by_user: string
+  type: string
+  report_data: any 
+  createdAt?: Date
+  updatedAt?: Date
 }
 export default ReportAttributes
--- src/models/report.ts
+++ src/models/report.ts
@@ -1,6 +1,7 @@
 import {DataTypes, Model, Optional} from 'sequelize'
-import ReportAttributes from './Interface/reportAttributes'
+import ReportAttributes from '../models/Interface/reportAttributes'
 import sequelize from '../database/db'
+
 type reportCreationAttributes = Optional<ReportAttributes, 'id_report'>
 
 class Report
@@ -9,18 +10,30 @@ class Report
 {
   declare id_report: number
   declare generated_by_user: string
+  declare type: string
+  declare report_data: any
+  declare createdAt: Date
+  declare updatedAt: Date
 }
+
 Report.init(
   {
     id_report: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true,
-      unique: true,
     },
     generated_by_user: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    report_data: {
+      type: DataTypes.JSON,
+      allowNull: true,
     },
   },
   {
