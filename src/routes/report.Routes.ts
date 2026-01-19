ee6f86bfde999320dbfee21e7986b69a36aaa3f7
Ivo Depari <ivodepari1302@hotmail.com>
2026-01-19 15:42:00 -0300

    agrego ruta para descargar los reportes

--- src/routes/report.Routes.ts
+++ src/routes/report.Routes.ts
@@ -4,10 +4,15 @@ import {
   getReportID,
   createReport,
   deleteReport,
+  downloadReportById,
 } from '../controllers/report.Controller'
+
 const router = Router()
+
 router.get('/', getReports)
 router.get('/:id', getReportID)
+router.get('/:id/download', downloadReportById)
 router.post('/', createReport)
 router.delete('/:id', deleteReport)
+
 export default router
